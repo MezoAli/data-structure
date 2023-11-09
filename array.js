393c21cf0d243a6a5a402ec4fbed9b8f1c48c3bd
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-09 11:27:39 +0200

    implement delete method for specific index

--- array.js
+++ array.js
@@ -24,10 +24,12 @@ class MyArray {
   }
 
   get(index) {
+    // O(1)
     return this.data[index];
   }
 
   push(item) {
+    // O(1)
     this.data[this.length] = item;
     this.length++;
     // as typical push method is js return length
@@ -35,10 +37,25 @@ class MyArray {
   }
 
   pop() {
+    // O (1)
     const lastItem = this.data[this.length - 1];
     delete this.data[this.length - 1];
     this.length--;
     return lastItem;
+  }
+
+  delete(index) {
+    // O(n)
+    if (index >= this.length) {
+      alert(`you only have ${this.length} length array`);
+      return;
+    }
+
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
   }
 }
 
@@ -49,4 +66,6 @@ strings.push("c");
 strings.push("d");
 strings.push("e");
 console.log(strings.pop());
+strings.delete(0);
+strings.delete(2);
 console.log(strings);
