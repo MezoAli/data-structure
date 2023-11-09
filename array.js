43a86b228399d0f1bc618db9c67f7f79d196d92b
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-09 10:49:54 +0200

    create array data structure with get,push and pop methods

--- array.js
+++ array.js
@@ -1,14 +1,52 @@
-class Player {
-  constructor(name, rule) {
-    this.name = name;
-    this.rule = rule;
+// class constructor
+
+// class Player {
+//   constructor(name, rule) {
+//     this.name = name;
+//     this.rule = rule;
+//   }
+
+//   introduce() {
+//     console.log(`hi, my name is ${this.name}`);
+//   }
+// }
+
+// const moutaz = new Player("Moutaz", "Coder");
+// console.log(moutaz);
+// moutaz.introduce();
+
+// Implement Array
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
   }
 
-  introduce() {
-    console.log(`hi, my name is ${this.name}`);
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    // as typical push method is js return length
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
   }
 }
 
-const moutaz = new Player("Moutaz", "Coder");
-console.log(moutaz);
-moutaz.introduce();
+const strings = new MyArray();
+strings.push("a");
+strings.push("b");
+strings.push("c");
+strings.push("d");
+strings.push("e");
+console.log(strings.pop());
+console.log(strings);
