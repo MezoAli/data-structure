433c6d702a093a1e6a4cd850d27914272a2bca5f
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-09 10:36:09 +0200

    refresh class knowledge in js

--- array.js
+++ array.js
@@ -1,1 +1,14 @@
-alert("hi");
+class Player {
+  constructor(name, rule) {
+    this.name = name;
+    this.rule = rule;
+  }
+
+  introduce() {
+    console.log(`hi, my name is ${this.name}`);
+  }
+}
+
+const moutaz = new Player("Moutaz", "Coder");
+console.log(moutaz);
+moutaz.introduce();
