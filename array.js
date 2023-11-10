cac3b848e6472ff88f17daa50fd02797c464dc38
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-10 15:35:23 +0200

    implement sortedArray Function

--- array.js
+++ array.js
@@ -72,23 +72,59 @@
 
 // Reverse String => Interview Question
 
-const reverseString = (str) => {
-  if (typeof str !== "string") {
-    alert("the input must be string");
-    return;
+// const reverseString = (str) => {
+//   if (typeof str !== "string") {
+//     alert("the input must be string");
+//     return;
+//   }
+//   const strArray = str.split("");
+//   console.log(strArray);
+//   let newArray = [];
+//   for (let i = strArray.length - 1; i >= 0; i--) {
+//     newArray.push(strArray[i]);
+//   }
+//   //   strArray.forEach((item) => newArray.push(item));
+//   const reversedString = newArray.join("");
+//   return reversedString;
+// };
+
+// console.log(reverseString("abc"));
+// console.log(reverseString("hi"));
+// console.log(reverseString("my name is moutaz"));
+// console.log(reverseString([]));
+
+// Merge 2 Arrays and sort them
+// [1,4,67,43] [2,5,9,32,104] => [1,2,4,5,9,32,43,67,104]
+
+const mergeTwoArraysSorted = (arr1, arr2) => {
+  const returedArray = [];
+
+  let array1Item = arr1[0];
+  let array2Item = arr2[0];
+  let i = 1;
+  let j = 1;
+
+  for (let z = 0; z < [...arr1, ...arr2].length; z++) {
+    if (array1Item < array2Item) {
+      returedArray.push(array1Item);
+      array1Item = arr1[i];
+      i++;
+    } else {
+      returedArray.push(array2Item);
+      array2Item = arr2[j];
+      j++;
+    }
   }
-  const strArray = str.split("");
-  console.log(strArray);
-  let newArray = [];
-  for (let i = strArray.length - 1; i >= 0; i--) {
-    newArray.push(strArray[i]);
-  }
-  //   strArray.forEach((item) => newArray.push(item));
-  const reversedString = newArray.join("");
-  return reversedString;
+  console.log(returedArray);
+
+  return returedArray;
 };
 
-console.log(reverseString("abc"));
-console.log(reverseString("hi"));
-console.log(reverseString("my name is moutaz"));
-console.log(reverseString([]));
+mergeTwoArraysSorted([0, 1, 4, 67, 43], [2, 5, 9, 32, 104, 198]);
+
+const mergeTwoArraysSorted2 = (arr1, arr2) => {
+  const newArrayUnSorted = [...arr1, ...arr2];
+  return newArrayUnSorted.sort((a, b) => a - b);
+};
+
+console.log(mergeTwoArraysSorted2([1, 4, 67, 43], [2, 5, 9, 32, 104]));
