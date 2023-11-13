e08a8bcf442ef164b3ee8650e3c417f32383ecf7
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-13 12:49:31 +0200

    create class LinkedList with append method

---
+++ linked-lists.js
@@ -0,0 +1,38 @@
+// 10 ---> 5 ----> 16
+
+// const myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = { value, next: null };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+
+console.log(myLinkedList);
