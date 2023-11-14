ba207c6fbd2fe77fd3fab0ed329f924208d08533
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-14 09:53:53 +0200

    add insert method

--- linked-lists.js
+++ linked-lists.js
@@ -43,6 +43,29 @@ class LinkedList {
     this.head = newNode;
     this.length++;
   }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode?.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      this.append(value);
+      return this;
+    }
+    let newNode = new Node(value);
+    let leader = this.traverseToIndex(index - 1);
+    let afterNode = leader?.next;
+    leader.next = newNode;
+    newNode.next = afterNode;
+    this.length++;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
@@ -50,5 +73,8 @@ myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
 myLinkedList.prepend(9);
+myLinkedList.insert(99, 23);
+myLinkedList.insert(2, 100);
+myLinkedList.insert(2, 101);
 
 console.log(myLinkedList);
