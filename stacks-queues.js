68762196d3e3cad8cc9e10cb548776af6677f96e
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-18 16:35:18 +0200

    add pop and peek method to stack class

--- stacks-queues.js
+++ stacks-queues.js
@@ -28,11 +28,33 @@ class Stack {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    const TopValue = this.top.value;
+    this.top = this.top.next;
+    this.length--;
+    return TopValue;
+  }
+
+  peek() {
+    if (this.length === 0) {
+      return this;
+    }
+    return this.top;
+  }
 }
 
 const myStack = new Stack();
 myStack.push("Google");
 myStack.push("Youtube");
 myStack.push("Discord");
+console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack.peek());
 
 console.log(myStack);
