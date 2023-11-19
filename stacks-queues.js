d5900612048d07654c60e6120d4b91a36324982a
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-19 09:32:44 +0200

    implement Queue class with enqueue,dequeue and peek method using linked list

--- stacks-queues.js
+++ stacks-queues.js
@@ -55,9 +55,9 @@ const myStack = new Stack();
 myStack.push("Google");
 myStack.push("Youtube");
 myStack.push("Discord");
-console.log(myStack.pop());
-console.log(myStack.pop());
-console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack.pop());
 // console.log(myStack.pop());
 // console.log(myStack.pop());
 // console.log(myStack.pop());
@@ -97,9 +97,56 @@ const myStack1 = new StackArray();
 myStack1.push("Google");
 myStack1.push("Youtube");
 myStack1.push("Discord");
-console.log(myStack1.peek());
-myStack1.pop();
-myStack1.pop();
-myStack1.pop();
+// console.log(myStack1.peek());
+// myStack1.pop();
+// myStack1.pop();
+// myStack1.pop();
 
-console.log(myStack1);
+// console.log(myStack1);
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first.value;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+      this.length++;
+      return this;
+    }
+    this.last.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (this.length === 0) {
+      return null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue("Google");
+myQueue.enqueue("Youtube");
+myQueue.enqueue("Discord");
+console.log(myQueue.peek());
+myQueue.dequeue();
+myQueue.dequeue();
+console.log(myQueue.peek());
+myQueue.dequeue();
+
+console.log(myQueue);
