4c1ed859cc648743cbdb5306a2ae40f4ebf57810
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-20 09:36:09 +0200

    implement Binary Search Tree class with insert method

---
+++ trees.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+//        9
+//    4      20
+//  1   6  15   170
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+}
+
+const myBinarySearchTree = new BinarySearchTree();
+myBinarySearchTree.insert(9);
+myBinarySearchTree.insert(4);
+myBinarySearchTree.insert(20);
+myBinarySearchTree.insert(1);
+myBinarySearchTree.insert(6);
+myBinarySearchTree.insert(15);
+myBinarySearchTree.insert(170);
+
+console.log(myBinarySearchTree);
